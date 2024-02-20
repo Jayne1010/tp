58cffc5cac3a750e8819108a21b1a994f9292ff7
jayne1010 <e0970489@u.nus.edu>
2024-02-20 21:32:07 +0800

    Tutorial-adding-command

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -90,10 +90,4 @@ public class PersonTest {
         assertFalse(ALICE.equals(editedAlice));
     }
 
-    @Test
-    public void toStringMethod() {
-        String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
-                + ", email=" + ALICE.getEmail() + ", address=" + ALICE.getAddress() + ", tags=" + ALICE.getTags() + "}";
-        assertEquals(expected, ALICE.toString());
-    }
 }
